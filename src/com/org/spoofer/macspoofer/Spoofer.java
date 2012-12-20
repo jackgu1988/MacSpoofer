e881f220b2a5af9d2ad3ddc139fbe0dfafdfbd8f
Jack Gurulian <jack.gurulian@gmail.com>
2012-12-20 03:13:34 +0200

    - A bit of code cleanup (some extra checks performed though because the method that checks if the device is rooted does not always return the correct value)

--- src/com/org/spoofer/macspoofer/Spoofer.java
+++ src/com/org/spoofer/macspoofer/Spoofer.java
@@ -61,156 +61,144 @@ public class Spoofer extends Activity {
 		final TextView error = (TextView) findViewById(R.id.Error);
 		final TextView current_mac = (TextView) findViewById(R.id.current_mac);
 
-		if (cmd.checkRoot())
-			if (!cmd.checkBusybox()) {
-				new AlertDialog.Builder(this)
-						.setIcon(android.R.drawable.ic_dialog_alert)
-						.setTitle("Error")
-						.setMessage(
-								"You do not seem to have busybox installed.\n Exiting...")
-						.setPositiveButton("OK",
-								new DialogInterface.OnClickListener() {
-									@Override
-									public void onClick(DialogInterface dialog,
-											int which) {
-										finish();
-									}
+		if (!cmd.checkRoot())
+			alert("You do not seem to have a rooted device.\n Exiting...");
 
-								}).show();
-			} else {
+		if (!cmd.checkBusybox())
+			alert("You do not seem to have busybox installed.\n Exiting...");
 
-				cmd.getRoot();
+		if (cmd.checkRoot()) {
+			cmd.getRoot();
 
-				try {
-					for (Enumeration<NetworkInterface> en = NetworkInterface
-							.getNetworkInterfaces(); en.hasMoreElements();) {
-						NetworkInterface intf = en.nextElement();
-						if (!intf.getDisplayName().equals("lo"))
-							ifaces.add(intf.getDisplayName());
-					}
-				} catch (SocketException e) {
+			try {
+				for (Enumeration<NetworkInterface> en = NetworkInterface
+						.getNetworkInterfaces(); en.hasMoreElements();) {
+					NetworkInterface intf = en.nextElement();
+					if (!intf.getDisplayName().equals("lo"))
+						ifaces.add(intf.getDisplayName());
 				}
-
-				final Spinner iface_list = (Spinner) findViewById(R.id.iface_selector);
-
-				ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(
-						this, android.R.layout.simple_spinner_item, ifaces);
-				dataAdapter
-						.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-				iface_list.setAdapter(dataAdapter);
-
-				for (int i = 0; i < ifaces.size(); i++) {
-					// Since wlan0 is a common name for the wireless interface
-					if (ifaces.get(i).equals("wlan0")) {
-						iface_list.setSelection(i);
-					}
-				}
-
-				iface_list
-						.setOnItemSelectedListener(new OnItemSelectedListener() {
-							@Override
-							public void onItemSelected(
-									AdapterView<?> parentView,
-									View selectedItemView, int position, long id) {
-								current_mac.setText("Current MAC: "
-										+ cmd.getCurrentMac(String
-												.valueOf(iface_list
-														.getSelectedItem())));
-							}
-
-							@Override
-							public void onNothingSelected(
-									AdapterView<?> parentView) {
-							}
-
-						});
-
-				current_mac.setText("Current MAC: "
-						+ cmd.getCurrentMac(String.valueOf(iface_list
-								.getSelectedItem())));
-
-				macField.addTextChangedListener(new TextWatcher() {
-
-					@Override
-					public void onTextChanged(CharSequence s, int start,
-							int before, int count) {
-
-						String pattern = "^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$";
-
-						if (!macField.getText().toString().matches(pattern)) {
-							error.setText("Wrong format!");
-							correctMac = false;
-						} else {
-							error.setText("");
-							correctMac = true;
-						}
-					}
-
-					@Override
-					public void beforeTextChanged(CharSequence s, int start,
-							int count, int after) {
-
-					}
-
-					@Override
-					public void afterTextChanged(Editable s) {
-
-					}
-				});
-
-				final Button buttonOK = (Button) findViewById(R.id.OK);
-				buttonOK.setOnClickListener(new View.OnClickListener() {
-					public void onClick(View v) {
-						if (correctMac) {
-							WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-							wifi.disconnect();
-
-							cmd.changeMac(macField.getText().toString(), String
-									.valueOf(iface_list.getSelectedItem()));
-
-							current_mac.setText("Current MAC: "
-									+ cmd.getCurrentMac(String
-											.valueOf(iface_list
-													.getSelectedItem())));
-						}
-					}
-				});
-
-				final Button buttonRnd = (Button) findViewById(R.id.Random);
-				buttonRnd.setOnClickListener(new View.OnClickListener() {
-					public void onClick(View v) {
-						Random rand = new Random();
-						String result = "";
-						for (int i = 0; i < 6; i++) {
-							int myRandomNumber = rand.nextInt(0xff) + 0x00;
-
-							if (myRandomNumber <= 15)
-								result += "0";
-
-							result += Integer.toHexString(myRandomNumber);
-
-							if (i < 5)
-								result += ":";
-						}
-						macField.setText(result);
-					}
-				});
+			} catch (SocketException e) {
 			}
-		else
-			new AlertDialog.Builder(this)
-					.setIcon(android.R.drawable.ic_dialog_alert)
-					.setTitle("Error")
-					.setMessage(
-							"You do not seem to have a rooted device.\n Exiting...")
-					.setPositiveButton("OK",
-							new DialogInterface.OnClickListener() {
-								@Override
-								public void onClick(DialogInterface dialog,
-										int which) {
-									finish();
-								}
 
-							}).show();
+			final Spinner iface_list = (Spinner) findViewById(R.id.iface_selector);
+
+			ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+					android.R.layout.simple_spinner_item, ifaces);
+			dataAdapter
+					.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			iface_list.setAdapter(dataAdapter);
+
+			for (int i = 0; i < ifaces.size(); i++) {
+				// Since wlan0 is a common name for the wireless interface
+				if (ifaces.get(i).equals("wlan0")) {
+					iface_list.setSelection(i);
+				}
+			}
+
+			iface_list.setOnItemSelectedListener(new OnItemSelectedListener() {
+				@Override
+				public void onItemSelected(AdapterView<?> parentView,
+						View selectedItemView, int position, long id) {
+					current_mac.setText("Current MAC: "
+							+ cmd.getCurrentMac(String.valueOf(iface_list
+									.getSelectedItem())));
+				}
+
+				@Override
+				public void onNothingSelected(AdapterView<?> parentView) {
+				}
+
+			});
+
+			current_mac.setText("Current MAC: "
+					+ cmd.getCurrentMac(String.valueOf(iface_list
+							.getSelectedItem())));
+
+			macField.addTextChangedListener(new TextWatcher() {
+
+				@Override
+				public void onTextChanged(CharSequence s, int start,
+						int before, int count) {
+
+					String pattern = "^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$";
+
+					if (!macField.getText().toString().matches(pattern)) {
+						error.setText("Wrong format!");
+						correctMac = false;
+					} else {
+						error.setText("");
+						correctMac = true;
+					}
+				}
+
+				@Override
+				public void beforeTextChanged(CharSequence s, int start,
+						int count, int after) {
+
+				}
+
+				@Override
+				public void afterTextChanged(Editable s) {
+
+				}
+			});
+
+			final Button buttonOK = (Button) findViewById(R.id.OK);
+			buttonOK.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					if (correctMac) {
+						WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+						wifi.disconnect();
+
+						cmd.changeMac(macField.getText().toString(),
+								String.valueOf(iface_list.getSelectedItem()));
+
+						current_mac.setText("Current MAC: "
+								+ cmd.getCurrentMac(String.valueOf(iface_list
+										.getSelectedItem())));
+					}
+				}
+			});
+
+			final Button buttonRnd = (Button) findViewById(R.id.Random);
+			buttonRnd.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					Random rand = new Random();
+					String result = "";
+					for (int i = 0; i < 6; i++) {
+						int myRandomNumber = rand.nextInt(0xff) + 0x00;
+
+						if (myRandomNumber <= 15)
+							result += "0";
+
+						result += Integer.toHexString(myRandomNumber);
+
+						if (i < 5)
+							result += ":";
+					}
+					macField.setText(result);
+				}
+			});
+		}
+	}
+
+	/**
+	 * Alerts for critical errors (missing busybox/root) with popup
+	 * 
+	 * @param msg
+	 *            the message to be displayed
+	 */
+	private void alert(String msg) {
+		new AlertDialog.Builder(this)
+				.setIcon(android.R.drawable.ic_dialog_alert).setTitle("Error")
+				.setMessage(msg)
+				.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						finish();
+					}
+
+				}).show();
 	}
 
 	@Override
